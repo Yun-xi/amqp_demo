4e1efb8941e7faf56b4b492d51c9337a31f56821
XieYaqi <987159036@qq.com>
2019-11-25 21:28:10 +0800

    init

---
+++ src/main/java/com/xxxx/amqpdemo/convert/ConverterBody.java
@@ -0,0 +1,22 @@
+package com.xxxx.amqpdemo.convert;
+
+public class ConverterBody {
+
+	private byte[] body;
+	
+	public ConverterBody() {
+	}
+
+	public ConverterBody(byte[] body) {
+		this.body = body;
+	}
+
+	public byte[] getBody() {
+		return body;
+	}
+
+	public void setBody(byte[] body) {
+		this.body = body;
+	}
+	
+}
